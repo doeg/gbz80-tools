88284918b0110d6b46948fafb78e5fac85baa11d
Sara Bee <hi@doeg.gy>
2018-03-18 11:01:40 -0400

    Add hex conversion

--- src/util/convert.js
+++ src/util/convert.js
@@ -1,3 +1,5 @@
+require('babel-polyfill')
+
 const rowTo2BPP = (row: number[]): Array<Array<number>> => {
   const byte0 = []
   const byte1 = []
@@ -12,3 +14,11 @@ const rowTo2BPP = (row: number[]): Array<Array<number>> => {
 
 export const to2BPP = (tile: Array<Array<number>>): Array<Array<number>> =>
   tile.map(row => rowTo2BPP(row))
+
+const binaryToHex = (binary: Array<number>): string =>
+  parseInt(binary.join(''), 2).toString(16).padStart(2, '0')
+
+const rowToHex = (row: Array<Array<number>>): string[] => row.map(binaryToHex)
+
+export const toHex = (tile: Array<Array<number>>): Array<string> =>
+  to2BPP(tile).reduce((acc, row) => acc.concat(rowToHex(row)), [])
