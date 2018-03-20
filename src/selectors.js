d716f24d8178e21cbe46ef0c8fd806b521ce4aae
Sara Bee <sara@turbinelabs.io>
2018-03-20 09:05:12 -0400

    Fix clear tile button

--- src/selectors.js
+++ src/selectors.js
@@ -1,5 +1,7 @@
 // @flow
-import type { AppState, Tile } from './types'
+import type { AppState, Tile, UUID } from './types'
+
+export const getActiveTileID = ({ activeTile }: AppState): ?UUID => activeTile
 
 export const getActiveTile = ({ activeTile, tiles }: AppState): ?Tile =>
   tiles.find(({ id }) => id === activeTile) || null
