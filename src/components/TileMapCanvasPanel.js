5ba4fd7c7813bc99a9fc781c7c0a5afed9fbf7f0
Sara Bee <sara@turbinelabs.io>
2018-03-23 18:16:36 -0400

    :skull: First implementation of TileMap render

--- src/components/TileMapCanvasPanel.js
+++ src/components/TileMapCanvasPanel.js
@@ -3,6 +3,7 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 
+import style from './tileMapCanvasPanel.css'
 import CanvasSizeInput from './CanvasSizeInput'
 import Panel from './Panel'
 import { getActiveTileMap } from '../selectors'
@@ -13,18 +14,36 @@ type MappedProps = {
 type Props = MappedProps
 
 class TileMapCanvasPanel extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props)
+    this.onClickCell = this.onClickCell.bind(this)
+  }
+
+  onClickCell(row, col) {
+    console.log('clicked', row, col)
+  }
+
   render() {
     const { tileMap } = this.props
     const title = tileMap ? `${tileMap.name} - Tile Map` : 'Tile Map'
 
     return (
-      <Panel height={500} id="TileMapCanvasPanel" title={title} width={750}>
+      <Panel height={600} id="TileMapCanvasPanel" title={title} width={540}>
         <CanvasSizeInput />
-        <pre>
-          {tileMap.map.map((row, idx) => (
-            <p key={idx}>{JSON.stringify(row)}</p>
+        <div className={style.grid}>
+          {tileMap.map.map((row, rdx) => (
+            <div className={style.row}>
+              {row.map((cell, cdx) => (
+                <div
+                  className={style.cell}
+                  onClick={() => this.onClickCell(rdx, cdx)}
+                >
+                  {tileMap.map[rdx][cdx]}
+                </div>
+              ))}
+            </div>
           ))}
-        </pre>
+        </div>
       </Panel>
     )
   }
