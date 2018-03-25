8b34f60af99980b1d8bb0d5df3e08daffbe08047
Sara Bee <sara@turbinelabs.io>
2018-03-24 21:08:00 -0400

    bleh

--- src/components/HotkeysPanel.js
+++ src/components/HotkeysPanel.js
@@ -5,9 +5,21 @@ import * as React from 'react'
 import style from './hotkeysPanel.css'
 import Panel from './Panel'
 
+const hotkeys = {
+  eraser: 'e',
+  pencil: 'p',
+}
+
 const HotkeysPanel = () => (
   <Panel height={540} id="HotkeysPanel" title="Hotkeys" width={200}>
-    hotkeys
+    <ul className={style.list}>
+      {Object.keys(hotkeys).map(label => (
+        <li key={label}>
+          <span className={style.label}>{label}</span>
+          <span className={style.hotkey}>{hotkeys[label]}</span>
+        </li>
+      ))}
+    </ul>
   </Panel>
 )
 
