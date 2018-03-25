0d69c4f045c7b4ccafc63ce2156f503829fb0e52
Sara Bee <sara@turbinelabs.io>
2018-03-24 22:05:26 -0400

    Add Icon component

---
+++ src/components/Icon.js
@@ -0,0 +1,42 @@
+// @flow
+/* eslint-disable */
+import * as React from 'react'
+import * as faIcons from 'react-icons/lib/fa'
+import * as ioIcons from 'react-icons/lib/io'
+
+// See:
+//  - https://github.com/gorangajic/react-icons/tree/master/fa
+//  - https://fontawesome.com/icons?d=gallery
+const icons = {
+  eraser: 'FaEraser',
+  fill: 'IoPaintbucket',
+  marquee: 'IoQrScanner',
+  pencil: 'FaPencil',
+  pointer: 'FaMousePointer',
+  wand: 'IoWand',
+}
+
+type Props = {
+  icon: string,
+}
+
+const Icon = ({ icon, ...props }: Props) => {
+  const iconComponent = icons[icon]
+  if (!iconComponent) {
+    console.warn('nonexistent icon', icon)
+    return null
+  }
+
+  let IconComponent = faIcons[iconComponent]
+  if (!IconComponent) {
+    IconComponent = ioIcons[iconComponent]
+  }
+
+  if (!IconComponent) {
+    return null
+  }
+
+  return <IconComponent {...props} />
+}
+
+export default Icon
