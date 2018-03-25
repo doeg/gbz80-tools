54019b0f0fbeae5e4dc57a52ba0388be55a1549a
Sara Bee <sara@turbinelabs.io>
2018-03-25 10:54:55 -0400

    Tidy up convert.js

--- src/util/convert.js
+++ src/util/convert.js
@@ -1,3 +1,4 @@
+// @flow
 require('babel-polyfill')
 
 const rowTo2BPP = (row: number[]): Array<Array<number>> => {
@@ -13,12 +14,16 @@ const rowTo2BPP = (row: number[]): Array<Array<number>> => {
 }
 
 export const to2BPP = (tile: Array<Array<number>>): Array<Array<number>> =>
+  // $FlowFixMe
   tile.map(row => rowTo2BPP(row))
 
 const binaryToHex = (binary: Array<number>): string =>
-  parseInt(binary.join(''), 2).toString(16).padStart(2, '0')
+  parseInt(binary.join(''), 2)
+    .toString(16)
+    .padStart(2, '0')
 
 const rowToHex = (row: Array<Array<number>>): string[] => row.map(binaryToHex)
 
 export const toHex = (tile: Array<Array<number>>): Array<string> =>
+  // $FlowFixMe
   to2BPP(tile).reduce((acc, row) => acc.concat(rowToHex(row)), [])
